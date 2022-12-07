b406690f907d142c2191f4cf4cb35be469a85813
beresdev <beresdev@gmail.com>
2022-12-06 18:13:36 -0600

    Se agrega el metodo de readFile con promesas, usando promistify y otro usando fs/promises. Los metodos no funcionan llamandolos desde main, se dejan comenados

--- md-links.js
+++ md-links.js
@@ -1,4 +1,5 @@
-let fs = require('fs');
+const fs = require('fs');
+
 
 const readFile = (path) => {
     fs.readFile(path, 'utf-8', (err, data) => {
@@ -10,5 +11,40 @@ const readFile = (path) => {
     })
 }
 
-
 module.exports = {readFile};
+
+
+
+
+//****usando promistify****//
+
+
+// const fs = require('fs/promises');
+// const util = require('util');
+
+// const readingFile= (path) => {
+//     const readFile = util.promisify(fs.readFile);
+//     readFile(path, 'utf-8')
+//         .then((data) => {
+//             console.log(data);
+//         })
+
+//         .catch((err) => {
+//             console.log('Error: ', err)
+//         });
+// }
+
+
+//****usando fs/promises****//
+
+// const fs = require('fs/promises');
+
+// fs.readFile('/home/beres/Laboratoria/Proyecto1_Cipher/DEV002-cipher/FAQ.md')
+//     .then(data => {
+//         console.log(data.toString())
+//     })
+//     .catch(error => {
+//         console.log(error)
+//     })
+
+// console.log('Ultima linea')

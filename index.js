c0368aa57e8e62b6927cd6012695803ec58c4808
beresdev <beresdev@gmail.com>
2022-12-27 15:57:21 -0600

    Se agrega el shebang y el bin en la configuración para hacer el módulo ejecutable

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+#!/usr/bin/env node
+
 const {mdLinks} = require('./mdlinks.js')
 
+let epath = './md_files/';
+let options = null;
+
+mdLinks(epath, options)
+.catch((error) => {
+    console.log("Sin resultados por error en ruta u opciones")
+})
 
 module.exports = {mdLinks};
 
